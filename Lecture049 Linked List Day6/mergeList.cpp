e87fcf0d9ce343c134dc544a74c2b74f5224c54e
Arnav004 <141306851+Arnav400@users.noreply.github.com>
2024-04-06 08:03:23 +0530

    Update mergeList.cpp

--- Lecture049 Linked List Day6/mergeList.cpp
+++ Lecture049 Linked List Day6/mergeList.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdc++.h>
+
 /************************************************************
 
     Following is the linked list node structure.
@@ -21,51 +23,86 @@
     };
 
 ************************************************************/
-
-void solve(Node<int>* first, Node<int>* second) {
+Node<int>* solve(Node<int>* first, Node<int>* second)
+// now according to our condition , first is the smaller one 
+{
     
-    
-    Node* curr1 = first;
-    Node* next1 = curr1 -> next;
-    
-    Node* curr2 = second;
-    Node* next2 = curr2 -> next;
-    
-    while(next1 != NULL && curr2 != NULL) {
-        
-        if( (curr2 -> data >= curr1 -> data ) 
-           && ( curr2 -> data <= next1 -> data)) {
-            
-            curr1 -> next = curr2;
-            curr2 -> next = next1;
-            curr1 = curr2;
-            curr2 = next2;
-        }
-        else {
-            
-        }
-        
-        
+    if(first->next==NULL)
+    {
+        first->next=second;
+        return first ; 
     }
     
+
+  
+
+    Node<int>* curr1 = first ; 
+    Node<int>* next1 = curr1->next ; 
+
+    Node<int>* curr2 = second ; 
+
+     Node<int>* next2 = curr2->next;
     
+    while( next1!=NULL && curr2!=NULL   )
+    {
+        if( (curr2->data>=curr1->data) && (curr2->data<=next1->data)   )
+     {
+       curr1->next = curr2 ;
+       next2=curr2->next;
+       curr2->next=next1;
+
+       curr1=curr2 ; 
+       curr2=next2;
+       /*
+       if(curr2==NULL)
+       {
+           return first ;
+       }
+*/
+      // next2=curr2->next;
+
+     }
+     else{
+         curr1=next1;
+         next1=next1->next;
+         if(next1==NULL)
+         {
+            curr1->next=curr2;
+            return first ; 
+         }
+
+     }
+
+    }
+
+
+return first ; 
+
+
+
+
 }
 
+// Node<int>* first and Node* ko same hi samaj lo 
 Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
-{
-    if(first == NULL)
-        return second;
-    
-    if(second == NULL)
-        return first;
-    
-    if(first -> data <= second -> data ){
-        solve(first, second);
+{ 
+      if(first==NULL)
+    {
+        return second ; 
+    }
+    if(second==NULL)
+    {
+        return first  ; 
+    }
+    // determine which list has the smaller starting element 
+    if(first->data <= second->data)
+    {
+       return  solve(first , second);
     }
     else
     {
-        solve(second, first);
+       return  solve( second, first );
     }
-    
-    
+
+    // Write your code here.
 }
